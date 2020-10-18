18f3d0ec03667c4958b895787b4844a6d98fe7c8
Norbert Bencsik <norbert.bencsik@stud.uni-corvinus.hu>
2020-10-18 21:39:42 +0200

    végreeeee

--- gyak06/gyak06/Form1.cs
+++ gyak06/gyak06/Form1.cs
@@ -9,19 +9,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Xml;
 
 namespace gyak06
 {
     public partial class Form1 : Form
     {
 
-        new BindingList<RateData>  Rates;
+        private BindingList<RateData> Rates;
         
 
         public Form1()
         {
-            InitializeComponent();
 
+            Rates = new BindingList<RateData>();
+            InitializeComponent();
+            string result = mnbezes();
+
+            dataGridView1.DataSource = Rates;
+            xmlezes(result);
+            chartozas();
+        }
+
+        private static string mnbezes()
+        {
             var mnbService = new MNBArfolyamServiceSoapClient();
 
             var request = new GetExchangeRatesRequestBody()
@@ -35,8 +47,52 @@ namespace gyak06
 
 
             var result = response.GetExchangeRatesResult;
+            return result;
+        }
 
-            dataGridView1.DataSource = Rates;
+        private void xmlezes(string result)
+        {
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+
+                var rate = new RateData();
+                Rates.Add(rate);
+
+
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+
+
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0)
+                    rate.Value = value / unit;
+            }
+            
+        }
+
+        private void chartozas()
+        {
+            chart1.DataSource = Rates;
+
+            var series = chart1.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chart1.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chart1.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
         }
     }
 }
