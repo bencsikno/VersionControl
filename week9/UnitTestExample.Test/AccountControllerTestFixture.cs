60ec9fd44b8cce14f50b0f724d04acd3fa309ad6
Norbert Bencsik <norbert.bencsik@stud.uni-corvinus.hu>
2020-11-22 19:16:50 +0100

    5ik

--- week9/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ week9/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -5,17 +5,27 @@ using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using UnitTestExample.Controllers;
 
 namespace UnitTestExample.Test
 {
-    class AccountControllerTestFixture
+     public class AccountControllerTestFixture
     {
         [Test]
         public void TestValidateEmail(string email, bool expectedResult)
         {
-                     
-               
+            var accountController = new AccountController();
+
+            
+            var actualResult = accountController.ValidateEmail(email);
+
+            
+            Assert.AreEqual(expectedResult, actualResult);
+
         }
 
+
+
     }
+
 }
